bb88fc2b23ef547fa39243da07d13d116cd4385e
Zain Kergaye <zain4utah@gmail.com>
2024-02-06 21:05:57 -0700

    Adding more becaus of class

--- src/HelloWorld/HelloWorld.java
+++ src/HelloWorld/HelloWorld.java
@@ -1,5 +1,34 @@
 public class HelloWorld {
   public static void main(String[] args) {
     System.out.println("Hello");
+    double ererere = doStuff(21);
+    mortIsLikeReallyOld(12332312);
+    System.out.println(weekFourQuizTest(new int[] {1,2,3,4,5,56,6,67,75465,123}));
   }
+
+
+
+  /**
+   * This is a very cool function 
+   *
+   * @param reeeeeee 
+   * @return double 
+   */
+  public static double doStuff(int reeeeeee){
+    return reeeeeee / 2;
+  }
+
+  public static void mortIsLikeReallyOld(int age){
+    int rererr = 0;
+    return; 
+  }
+
+  public static int weekFourQuizTest(int[] bananas[]){
+    return 3;
+  }
+
+
+
+
+  
 }
--- src/newPackage/newPackage.java
+++ src/newPackage/newPackage.java
@@ -1,10 +1,36 @@
 package newPackage;
 
+import java.util.InputMismatchException;
+import java.util.Scanner;
+
 public class newPackage {
 
   public static void main(String[] args) {
+    // int integer = 3;
+    // double doubl = 3.5;
+    // System.out.println(integer + doubl);
+    // for(int i = 10; i >= 0; i--){
+    //   System.out.println(i);
+    // }
+    // int[] arr = new int[1000];
+    //
 
-    System.out.println("hello World");
-     
+    for(int i = 20; i < 0; i -= 5){
+      System.out.print(i);
+    }
+
+    Scanner input = new Scanner(System.in);
+    try{
+    int points = input.nextInt();
+    }
+    catch(InputMismatchException err){
+      int points = 0;
+    }
+    System.out.println(points + " points");
+    // This code is wrong because try catch variable initialization is only within that scope. Like initializing a variable inside an if loop.
+    if(true){
+      int pointss = 0;
+    }
+    System.out.println(pointss);
   }
 }
